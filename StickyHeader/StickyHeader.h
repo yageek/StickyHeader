ee4640f7543d80c2d7cf84d16031497686ec44ca
Yannick Heinrich <yannick.heinrich@gmail.com>
2016-08-24 17:13:41 +0200

    Initial Commit

---
+++ StickyHeader/StickyHeader.h
@@ -0,0 +1,19 @@
+//
+//  StickyHeader.h
+//  StickyHeader
+//
+//  Created by Yannick Heinrich on 24.08.16.
+//  Copyright © 2016 yageek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StickyHeader.
+FOUNDATION_EXPORT double StickyHeaderVersionNumber;
+
+//! Project version string for StickyHeader.
+FOUNDATION_EXPORT const unsigned char StickyHeaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StickyHeader/PublicHeader.h>
+
+
